1218be09a5dd4c3d0f2e6971d0d8ee1873259761
srinadh1304 <srinadh1304@gmail.com>
2021-09-08 20:00:02 +0530

    ADDED: Method to validate first name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class UserRegistration {
+	
+	public static boolean nameValidation(String name) {
+		String nameValidation="^[A-Z][a-z]{2,}$";
+		return Pattern.matches(nameValidation, name);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to User registration program");
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter First name");
+		String firstName=scanner.next();
+		if(!nameValidation(firstName))
+			System.out.println("Invalid format");
+		else
+			System.out.println("Valid format");
+		scanner.close();
 	}
 }
