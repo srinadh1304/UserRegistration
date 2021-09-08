7f1506321db9a4aba4a8107ba058ada9686e4e40
srinadh1304 <srinadh1304@gmail.com>
2021-09-08 19:53:43 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.userregistration;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User registration program");
+	}
+}
