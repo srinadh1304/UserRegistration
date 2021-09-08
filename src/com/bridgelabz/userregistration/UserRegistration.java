4ffcc1b1e2f8a21a8688440fdfab64c32b0e7ab7
srinadh1304 <srinadh1304@gmail.com>
2021-09-08 20:44:30 +0530

    MODIFIED: checking for all email id

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -67,6 +67,13 @@ public class UserRegistration {
 		else
 			System.out.println("Invalid");
 		
+		String[] emails= {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com",
+				"abc@gmail.com.com","abc+100@gmail.com","abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com",
+				"abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
+		for(int i=0;i<emails.length;i++) {
+			System.out.println(checkEmail(emails[i]));
+		}
+		
 		scanner.close();
 	}
 }
