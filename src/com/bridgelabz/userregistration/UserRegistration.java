8a7077b8dcad9ddf342e7b84453111e66577600f
srinadh1304 <srinadh1304@gmail.com>
2021-09-08 20:03:47 +0530

    MODIFIED: validating last name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -13,8 +13,16 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter First name");
-		String firstName=scanner.next();
+		String firstName=scanner.nextLine();
 		if(!nameValidation(firstName))
+			System.out.println("Invalid format");
+		else
+			System.out.println("Valid format");
+		
+
+		System.out.println("Enter Last name");
+		String lastName=scanner.nextLine();
+		if(!nameValidation(lastName))
 			System.out.println("Invalid format");
 		else
 			System.out.println("Valid format");
