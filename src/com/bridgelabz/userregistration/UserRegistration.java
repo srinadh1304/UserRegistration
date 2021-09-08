35bf4719f8d006830fbc838ed9f117b4f87ae7c1
srinadh1304 <srinadh1304@gmail.com>
2021-09-08 20:08:59 +0530

    MODIFIED: Validating email

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -10,22 +10,35 @@ public class UserRegistration {
 		return Pattern.matches(nameValidation, name);
 	}
 
+	public static boolean checkEmail(String email) {
+		Pattern pattern = Pattern.compile("([a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9])(([+_.-][a-zA-Z0-9]*)?)(@[a-zA-Z0-9]+)([.])([a-z]{2,})(([.][a-z]{2,})?)");
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+	
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter First name");
 		String firstName=scanner.nextLine();
 		if(!nameValidation(firstName))
-			System.out.println("Invalid format");
+			System.out.println("Invalid ");
 		else
-			System.out.println("Valid format");
+			System.out.println("Valid ");
 		
 
 		System.out.println("Enter Last name");
 		String lastName=scanner.nextLine();
 		if(!nameValidation(lastName))
-			System.out.println("Invalid format");
+			System.out.println("Invalid ");
 		else
-			System.out.println("Valid format");
+			System.out.println("Valid ");
+		
+		System.out.println("Enter email:");
+		String email=scanner.nextLine();
+		if(checkEmail(email))
+			System.out.println("Valid");
+		else 
+			System.out.println("Invalid");
 		scanner.close();
 	}
 }
