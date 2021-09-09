4db1942a01733f03c1e43be52f11322d47c7569f
srinadh1304 <srinadh1304@gmail.com>
2021-09-09 07:44:49 +0530

    REFACTORED: Removed spaces

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.*;
 
 public class UserRegistration {
 	
-	public static boolean nameValidation(String name) {
+	public static boolean checkName(String name) {
 		String nameValidation="^[A-Z][a-z]{2,}$";
 		return Pattern.matches(nameValidation, name);
 	}
@@ -14,7 +14,7 @@ public class UserRegistration {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter First name");
 		String firstName=scanner.nextLine();
-		if(!nameValidation(firstName))
+		if(!checkName(firstName))
 			System.out.println("Invalid format");
 		else
 			System.out.println("Valid format");
@@ -22,7 +22,7 @@ public class UserRegistration {
 
 		System.out.println("Enter Last name");
 		String lastName=scanner.nextLine();
-		if(!nameValidation(lastName))
+		if(!checkName(lastName))
 			System.out.println("Invalid format");
 		else
 			System.out.println("Valid format");
